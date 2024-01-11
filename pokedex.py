a5b43b0658af391574918fdb5c3dfcf8447e19f2
sersang <sersang01@gmail.com>
2024-01-11 14:58:05 +0800

    Added the pokedex.txt file and added functionality of searching up pokemons

--- pokedex.py
+++ pokedex.py
@@ -4,3 +4,17 @@ print("Hello welcome to the Pokedex")
 
 name = input("Enter Pokemon Name:")
 print("Pokemon is: " + name)
+pokemons=[]
+
+with open("pokedex.txt", 'r') as file:
+    for line in file:
+        pokemons.append(line.split(","))   
+    
+pokedex = {}
+for line in pokemons:
+    pokedex[line[1]] = line
+
+try:
+    print(pokedex[name])
+except:
+    print("Pokemon not available")
