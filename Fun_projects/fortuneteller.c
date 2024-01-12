ff6364b6bdf4a53eb0374b0c9b9e848691622295
sersang <sersang01@gmail.com>
2024-01-12 13:36:30 +0800

    Organized project folders

---
+++ Fun_projects/fortuneteller.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]){
+    int number =0;
+    
+    switch (number)
+    {
+    case 1:
+        printf("Hop on that train and and see where the railroad takes you \n");
+        break;
+    case 2:
+        printf("You are a very handy person");
+        break;
+    case 3:
+        printf("You are a very handy person");
+        break;
+    case 4:
+        printf("You are a very handy person");
+        break;
+    case 5:
+        printf("You are a very handy person");
+        break;
+    case 6:
+        printf("You are a very handy person");
+        break;
+    case 7:
+        printf("You are a very handy person");
+        break;
+    case 8:
+        printf("You are a very handy person");
+        break;
+    case 9:
+        printf("You are a very handy person");
+        break;
+    case 10:
+        printf("You are a very handy person");
+        break;
+    
+    default:
+        break;
+    }
+
+}
